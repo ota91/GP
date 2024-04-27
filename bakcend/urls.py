307852e3eedb52c83a037033a5419a6e10365a22
sulimanalkhunaizan <109586834+sulimanalkhunaizan@users.noreply.github.com>
2024-04-27 18:06:59 +0300

    Add files via upload

---
+++ bakcend/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from .views import UserRegistrationView, LogoutView, LoginView
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
